ac8afff1835a72a43bd9fd8e81101c82a91aab29
Mdsadiq03 <mohamedsadiq2k3@gmail.com>
2024-03-31 20:57:02 +0530

    Feat: User signing up

--- chat_code/screens/auth.dart
+++ chat_code/screens/auth.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final _firebase = FirebaseAuth.instance;
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -15,13 +18,32 @@ class _AuthScreenState extends State<AuthScreen> {
   var _enteredEmail = '';
   var _enteredPassword = '';
 
-  void _submit(){
+  void _submit() async {
     final isValid = _form.currentState!.validate();
 
-    if (isValid){
-      _form.currentState!.save();
-      print(_enteredEmail);
-      print(_enteredPassword);
+    if (!isValid) {
+      return;
+    }
+
+    _form.currentState!.save();
+
+    if (_isLogin) {
+    } else {
+      try {
+        final userCredentials = await _firebase.createUserWithEmailAndPassword(
+            email: _enteredEmail, password: _enteredPassword);
+        print(userCredentials);    
+      } on FirebaseAuthException catch (error) {
+        if (error.code == 'email-already-in-use') {
+          //..
+        }
+        ScaffoldMessenger.of(context).clearSnackBars();
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(error.message ?? 'Authentication failed.'),
+          ),
+        );
+      }
     }
   }
 
@@ -69,7 +91,7 @@ class _AuthScreenState extends State<AuthScreen> {
 
                               return null;
                             },
-                            onSaved: (value){
+                            onSaved: (value) {
                               _enteredEmail = value!;
                             },
                           ),
@@ -77,13 +99,13 @@ class _AuthScreenState extends State<AuthScreen> {
                             decoration:
                                 const InputDecoration(labelText: 'password'),
                             obscureText: true,
-                            validator: (value){
-                              if (value == null || value.trim().length < 6){
+                            validator: (value) {
+                              if (value == null || value.trim().length < 6) {
                                 return 'Password must be atleast 6 characters long.';
                               }
                               return null;
                             },
-                            onSaved: (value){
+                            onSaved: (value) {
                               _enteredPassword = value!;
                             },
                           ),
